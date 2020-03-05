508942a568e548e494a8ca17c8534116c4081aa8
hojoungjang <jang88@purdue.edu>
2020-03-04 20:03:05 -0500

    Add custom resize and totensor function back

--- ctc/TextLocalizationSet.py
+++ ctc/TextLocalizationSet.py
@@ -59,11 +59,10 @@ class TextLocalizationSet(Dataset):
         """
         Allows indexing of the dataset ==> ex. dataset[0] returns a single sample from the dataset
         :param idx: index value of dataset sample we want to return
-        :return: dictionary with two keys: "image" and "gt"
+        :return: dictionary with three keys: "image", "coords" and "transcription"
                  value to "image": ndarray of pixel values
-                 value to "gt": list of dictionary with two keys: "coords" and "transcription"
-                                value to "coords": 2d array of x, y coordinates (i.e [ [x1, y1]...[x4, y4]])
-                                value to "transcription": string denoting actual word in image
+                 value to "coords": 2d array of x, y coordinates (i.e [ [x1, y1]...[x4, y4]])
+                 value to "transcription": string denoting actual word in image
         """
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -119,6 +118,67 @@ class TextLocalizationSet(Dataset):
         # perform transforms
         if self.transform:
             sample = self.transform(sample)
+
+        return sample
+
+
+class Rescale(object):
+    """ Rescale the image in a sample to a given size.
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+        matched to output_size. If int, smaller of image edges is matched
+        to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, coords = sample['image'], sample['coords']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = transform.resize(image, (new_h, new_w))
+
+        # h and w are swapped for landmarks because for images,
+        # x and y axes are axis 1 and 0 respectively
+        for i in range(len(coords)):
+            coords[i] = np.array(coords[i], dtype=float) * [new_w / w, new_h / h]
+            coords[i] = coords[i].round(0)
+            coords[i] = np.array(coords[i], dtype=int)
+
+        sample["image"] = img
+        sample["coords"] = coords
+
+        return sample
+
+
+class ToTensor(object):
+    """ Convert ndarrays in sample to Tensors. """
+
+    def __call__(self, sample):
+        image, coords = sample['image'], sample['coords']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+
+        for i in range(len(coords)):
+            coords[i] = torch.from_numpy(coords[i])
+
+        sample["image"] = torch.from_numpy(image)
+        sample["coords"] = coords
 
         return sample
 
